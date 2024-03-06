7d3232b28d799b7af7e651469291a587c1c141c3
mandiseli <mandiseli@outlook.com>
2024-03-06 13:34:22 +0200

    added a current weather component

---
+++ weather-app/src/components/current-weather/current-weather.js
@@ -0,0 +1,15 @@
+import "./current-weather.css"
+import React from 'react'
+
+const CurrentWeather = () => {
+  return (
+    <div className="weather">
+        <div className="top">
+            <p className="city">Cape Town</p>
+            <p className="weather-description">Sunny</p>
+        </div>
+    </div>
+  )
+}
+
+export default CurrentWeather
