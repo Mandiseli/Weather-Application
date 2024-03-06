d734566e78c9826d6f71e3361c3ba397ed25680a
mandiseli <mandiseli@outlook.com>
2024-03-06 10:09:46 +0200

    Added a search component

---
+++ weather-app/src/api.js
@@ -0,0 +1,15 @@
+//const url = 'https://wft-geo-db.p.rapidapi.com/v1/geo/cities';
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'ca00f38c17mshb117b78f0e2b395p1bc1a3jsn13c34adc0e2e',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo"
+
+
+
+
+	
